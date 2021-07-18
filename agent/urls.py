d7787fdef900c6e62973aead5588d8b50df5ecaa
amanansari10106 <69719509+amanansari10106@users.noreply.github.com>
2021-07-18 16:05:33 +0530

    DD

---
+++ agent/urls.py
@@ -0,0 +1,12 @@
+from django.urls.conf import path
+from django.urls.resolvers import URLPattern
+from agent import views
+
+urlpatterns =[
+    path("register/",views.becomeAgent.as_view()),
+    path("detail/",views.agentdetail.as_view())
+]
+
+
+
+
