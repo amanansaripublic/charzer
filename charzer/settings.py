d5272949d84c5e8536d244a5670d303b77f6520b
amanansari10106 <69719509+amanansari10106@users.noreply.github.com>
2021-07-18 18:40:02 +0530

    FDF

--- charzer/settings.py
+++ charzer/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'charzer.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'dtbase/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
